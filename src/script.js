212a01a5376eb1d9a8fade586de0c8f4fbb3daf1
jungjy0523 <147782436+jungjy0523@users.noreply.github.com>
2024-01-03 15:59:49 +0900

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
+const temp = document.createElement("div");
+temp.innerHTML = `<div class="action">
+  <button>+</button>
+  <ul>
+    <li><a href="https://support.portal.megaone.com/servicedesk/customer/portal/66/create/824">General Support</a></li>
+	<li><a href="https://support.portal.megaone.com/servicedesk/customer/portal/66/create/826">Billing Support</a></li>
+	<li><a href="https://support.portal.megaone.com/servicedesk/customer/portal/66/create/825">Technical Support</a></li>
+  </ul>
+</div>`;
+document.body.appendChild(temp);
+
 const action = document.querySelector('.action');
 action.addEventListener('click', ()=> {
   action.classList.toggle('active')
